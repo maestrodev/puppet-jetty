ec4aaffb843d8063e8659a4ee40327a475cc5908
Kit Plummer <kitplummer@gmail.com>
2011-08-31 17:21:54 -0400

    setting up service

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,22 @@
 class jetty {
-  
-  exec { "jetty_download":
-    command => "wget http://download.eclipse.org/jetty/$jetty_version/dist/jetty-distribution-$jetty_version.tar.gz",
-    cwd     => "/usr/local/src",
-    creates => "/usr/local/src/jetty-distribution-$jetty_version.tar.gz",
-    path    => ["/usr/bin", "/usr/sbin"],
-    require => [Group["activemq"],Package["java-1.6.0-openjdk-devel"]],
-  }
-  
+  include wget
+  wget::fetch { "jetty_download":
+    source => "wget http://download.eclipse.org/jetty/$jetty_version/dist/jetty-distribution-$jetty_version.tar.gz",
+    destination => "/usr/local/src/jetty-distribution-$jetty_version.tar.gz",
+  } ->
+  #exec { "jetty_download":
+  #  command => "wget http://download.eclipse.org/jetty/$jetty_version/dist/jetty-distribution-$jetty_version.tar.gz",
+  #  cwd     => "/usr/local/src",
+  #  creates => "/usr/local/src/jetty-distribution-$jetty_version.tar.gz",
+  #  path    => ["/usr/bin", "/usr/sbin"],
+  #  require => [Group["activemq"],Package["java-1.6.0-openjdk-devel"]],
+  #}
   exec { "jetty_untar":
     command => "tar xf /usr/local/src/jetty-distribution-$jetty_version.tar.gz && chown -R $jetty_user:$jetty_group $jetty_home/jetty-distribution-$jetty_version",
     cwd     => "$jetty_home",
     creates => "$jetty_home/jetty-distribution-$jetty_version",
     path    => ["/bin",],
-    require => Exec["jetty_download"],
-#also need to chown activemq:activemq this dir
+    notify => Service["jetty"]
   }
   
   file { "$jetty_home/jetty":
@@ -32,9 +34,10 @@ class jetty {
   }
 
   service {"jetty":
+    enable => true,
     ensure => running,
+    hasstatus => false,
     hasrestart => true,
-    require    => [File["/etc/init.d/jetty"],Exec["jetty_untar"],Exec["jetty_download"],File["$jetty_home/jetty"],File["/var/log/jetty"]],
   }
   
 }
