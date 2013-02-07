6eb14628e20cd3aca5c6b83ae80a7df2635f7312
Brett Porter <brett@apache.org>
2013-02-07 17:42:32 +1100

    Remove old commented code

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,15 +4,6 @@ class jetty {
     source => "http://archive.eclipse.org/jetty/$jetty_version/dist/jetty-distribution-$jetty_version.tar.gz",
     destination => "/usr/local/src/jetty-distribution-$jetty_version.tar.gz",
   } ->
-  #exec { "jetty_download":
-  #  command => "wget http://download.eclipse.org/jetty/$jetty_version/dist/jetty-distribution-$jetty_version.tar.gz",
-  #  cwd     => "/usr/local/src",
-  #  creates => "/usr/local/src/jetty-distribution-$jetty_version.tar.gz",
-  #  path    => ["/usr/bin", "/usr/sbin"],
-  #  require => [Group["activemq"],Package["java-1.6.0-openjdk-devel"]],
-  #}
-  
-
   exec { "jetty_untar":
     command => "tar xf /usr/local/src/jetty-distribution-$jetty_version.tar.gz && chown -R $jetty_user:$jetty_group $jetty_home/jetty-distribution-$jetty_version",
     cwd     => "$jetty_home",
