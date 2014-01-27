eae457937df89b45a1e457bc42d0bcd28e07627e
Carlos Sanchez <csanchez@maestrodev.com>
2014-01-27 23:34:38 +0100

    Update gems and add compile test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -9,6 +9,8 @@ describe 'jetty' do
     :group   => "jettygroup"
   }}
 
+  it { should compile.with_all_deps }
+
   it { should contain_file("/opt/jetty").with_ensure("/opt/jetty-distribution-9.0.4.v20130625") }
   it { should contain_service("jetty").with_ensure("running") }
   it { should contain_exec("jetty_untar").with_command(/chown -R jettyuser:jettygroup/) }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,9 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 
 Puppet::Util::Log.level = :warning
 Puppet::Util::Log.newdestination(:console)
+
+RSpec.configure do |c|
+  c.default_facts = {
+    :operatingsystem => 'centos',
+  }
+end
